8db478ee80266c0d2773fbd179487423d01b86ea
Berat <brtozturk01@gmail.com>
2022-06-28 11:58:11 +0300

    .

--- index.js
+++ index.js
@@ -1,10 +1,3 @@
-// var sayı1 = 4
-// console.log(sayı1)
-// var sayı2 = 15
-// console.log(sayı2)
-// var toplam = sayı1 + sayı2
-// console.log(toplam)
-// console.log(sayı1 + sayı2)
 const menuButton = document.querySelector(".menuButton")
 const menu = document.querySelector(".mobileMenu")
 const close = document.querySelector(".close")
