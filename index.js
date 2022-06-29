f4d1457aa5ef9572ad70d0016140d6aaee044e56
Berat <brtozturk01@gmail.com>
2022-06-29 20:05:12 +0300

    .

--- index.js
+++ index.js
@@ -1,11 +1,6 @@
 const menuButton = document.querySelector(".menuButton")
 const menu = document.querySelector(".mobileMenu")
 const close = document.querySelector(".close")
-console.log(menuButton)
-menuButton.onclick = () => {
-  console.log("tıklandı")
-  menu.style.display = "flex"
-}
-close.onclick = () => {
-  menu.style.display = "none"
-}
+menuButton.onclick = () => menu.style.display = "flex"
+
+close.onclick = () => menu.style.display = "none"
